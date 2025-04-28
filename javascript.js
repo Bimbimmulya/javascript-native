c63b7581468c81245062b7b437b551fbe4332620
bim.krismon <bim.krismon@permatabank.co.id>
2025-04-28 10:39:47 +0700

    dev[arr] - fungsi concat pada array: concat untuk menggabungkan nilai array

--- javascript.js
+++ javascript.js
@@ -1,27 +1,31 @@
+let loginForm = document.getElementById('loginForm');
 
-let loginForm = document.getElementById("loginForm");
-
-loginForm.addEventListener("submit", (e) => {
+loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('siniii ')
+  console.log('siniii ');
 
-  let username = document.getElementById("username");
-  console.log(username, '<--- username')
+  let username = document.getElementById('username');
+  console.log(username, '<--- username');
 
-  const nameAlreadyExist = 'supakul'
-  const newName = nameAlreadyExist.toLowerCase()
+  const nameAlreadyExist = 'supakul';
+  const newName = nameAlreadyExist.toLowerCase();
 
   if (username.value.toLowerCase() === newName) {
-    console.log('username already exist, please gunakan username yg lain')
+    console.log('username already exist, please gunakan username yg lain');
   } else {
-    if (username.value.length >= 6 && username.value.indexOf(' ') === -1 ) {
-        console.log('password valid')
+    if (username.value.length >= 6 && username.value.indexOf(' ') === -1) {
+      console.log('password valid');
     } else {
-      console.log('password minimal 6 karakter')
+      console.log('password minimal 6 karakter');
     }
   }
 });
 
-// struktur data Array 
+// struktur data Array
 
-let arr = [1, 'satu', true, false, null]
+let arr = [1, 'satu', true, false, null];
+
+const arr1 = ['a', 'b', 'c'];
+const arr2 = ['d', 'e', 'f']
+
+const arr3 = arr1.concat(arr2) // concat untuk menggabungkan nilai array
