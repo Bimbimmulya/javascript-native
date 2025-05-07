1f186e9e1f9e992c95319290469c263f0f10b03b
bim.krismon <bim.krismon@permatabank.co.id>
2025-05-07 15:06:39 +0700

    dev[about_function] - multiple argument & paramater

--- javascript.js
+++ javascript.js
@@ -1,10 +1,27 @@
-//paramater & argument
+//multiple paramater & argument
+//sometimes argumentnya harus di isi semua, sometimes tidak perlu di isi semua
 
-//paramater adalah sebuah data penampung argument
-//argument adalah sebuah data yg dikirim ke paramater
-//argument yg dikirim kedalam paramater bisa digunakan/dimanipulasi didalam function
-greeting = (data) => {
-  console.log(`Halo selamat siang ${data}`) //dalam blok scope ini kita bisa menggunakan/memanipulasi argument yg dikirim kedalam paramater
+//contoh harus diisi semua
+increament = (a, b) => {
+  const increas = a + b;
+  console.log(increas, '<---- increas')
 }
 
-greeting('john')
+//contoh tidak harus di isi 
+increament2 = (a = 0, b = 0) => { // tidak harus di isi karena sudah ada nilai paramater default
+  const increas = a + b;
+  console.log(increas, '<--- increas 2')
+}
+
+//contoh tidak harus di isi
+cekUserID = (data = '') => {
+  console.log(data, '<--- data')
+
+  if (data) {
+    console.log('show user ID')
+  } else {
+    console.log('hide user ID')
+  }
+
+  //pada kasus ini tidak harus di isi tapi tergantung penyikapan argument tersebut, yg harus diperhatikan adalah tidak boleh terjadi errorr ketika tidak mengirim argument, atau argument yg dikirim tidak sesuai
+}
